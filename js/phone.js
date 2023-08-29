27087a9aba674a4bf832a0ad2205206a11a31bde
Fahmida Faiza <fahmidafaiza57@gmail.com>
2023-08-30 00:46:07 +0600

    added dynamic image and name

--- js/phone.js
+++ js/phone.js
@@ -5,8 +5,37 @@ const loadPhone = async () =>{
 
     const data = await res.json();
    const phones =data.data;
-   console.log(phones);
-
+//    console.log(phones);
+        displayPhones(phones)
 }
+const displayPhones = phones =>{
 
+    // console.log(phones);
+    //step 1: set  korlam jeykahney boshabo
+const phoneContainer = document.getElementById("phone-container");
+    phones.forEach(phone => {
+        console.log(phone);
+        //step 2:create a div
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = `card w-96 bg-gray-100 shadow-xl`;
+        //step3: set innerhtml
+        phoneCard.innerHTML = `
+         <figure class="px-10 pt-10">
+                <img src="${phone.image}" alt="Shoes" class="rounded-xl" />
+            </figure>
+            <div class="card-body items-center text-center">
+                <h2 class="card-title">${phone.phone_name}</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div class="card-actions">
+                    <button class="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
+        
+        
+        `;
+
+        //stpe4:appendchild
+        phoneContainer.appendChild(phoneCard)
+    })
+}
 loadPhone();
